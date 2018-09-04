4c0b070c8250fa3d2050f0396f127443ba43621e
Olivier Giroux <ogiroux@gmail.com>
2018-09-04 09:36:47 -0700

    oops, broken the builds
    This is the beginning of a history rewrite annotation.
    This is a rewrite of a commit initially made in the main repo.
    Original hash: b9b48895443a0b713a594de2eb6d9f3fa46b7edc.

--- samples/linux.sh
+++ samples/linux.sh
@@ -1,1 +1,1 @@
-nvcc -I../include -gencode arch=compute_70 -std=c++11 -O2 trie.cu --expt-relaxed-constexpr -o trie
+nvcc -I../include -arch=compute_70 -std=c++11 -O2 trie.cu --expt-relaxed-constexpr -o trie
--- samples/mac.sh
+++ samples/mac.sh
@@ -1,1 +1,1 @@
-/Developer/NVIDIA/CUDA-9.2/bin/nvcc -I../include -gencode arch=compute_70 -std=c++11 -O2 trie.cu --compiler-bindir /usr/local/opt/llvm@5/bin/clang --expt-relaxed-constexpr -o trie
+/Developer/NVIDIA/CUDA-9.2/bin/nvcc -I../include -arch=compute_70 -std=c++11 -O2 trie.cu --compiler-bindir /usr/local/opt/llvm@5/bin/clang --expt-relaxed-constexpr -o trie
