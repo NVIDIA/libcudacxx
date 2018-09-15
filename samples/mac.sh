8957d0656e6a442c820c38e2784db5bf8b9d272d
Olivier Giroux <ogiroux@gmail.com>
2018-09-15 09:32:29 -0700

    make the other two samples on mac
    This is the beginning of a history rewrite annotation.
    This is a rewrite of a commit initially made in the main repo.
    Original hash: d01f21dd362d7770df7f9621fb812652ace8bc32.

--- samples/mac.sh
+++ samples/mac.sh
@@ -1,1 +1,3 @@
+clang++ -std=c++14 -O2 trie_st.cpp -o trie_st
+clang++ -std=c++14 -O2 trie_mt.cpp -o trie_mt
 /Developer/NVIDIA/CUDA-9.2/bin/nvcc -I../includes -arch=compute_70 -std=c++11 -O2 trie.cu --compiler-bindir /usr/local/opt/llvm@5/bin/clang --expt-relaxed-constexpr -o trie
