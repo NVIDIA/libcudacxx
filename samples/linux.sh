f8592d0c8f9eb9e60b2b69228e9e8bb42a62c198
Olivier Giroux <ogiroux@gmail.com>
2019-01-28 14:21:11 -0800

    moved back to the regular include name
    This is the beginning of a history rewrite annotation.
    This is a rewrite of a commit initially made in the main repo.
    Original hash: 32cec6dd55e315399de9eced080c2675b158bf31.

--- samples/linux.sh
+++ samples/linux.sh
@@ -1,3 +1,3 @@
 g++ -std=c++11 trie_st.cpp -O2 -o trie_st
 g++ -std=c++11 trie_mt.cpp -O2 -o trie_mt -pthread
-/usr/local/cuda/bin/nvcc -I../includes -arch=compute_70 -std=c++11 -O2 trie.cu --expt-relaxed-constexpr -o trie
+/usr/local/cuda/bin/nvcc -I../include -arch=compute_70 -std=c++11 -O2 trie.cu --expt-relaxed-constexpr -o trie
