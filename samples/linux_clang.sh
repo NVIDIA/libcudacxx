f3f48cf54fd6b8daea7a0d1dca2a3419ac83faac
Olivier Giroux <ogiroux@gmail.com>
2019-02-02 12:14:39 -0800

    Simplify some of the paths used.
    This is the beginning of a history rewrite annotation.
    This is a rewrite of a commit initially made in the main repo.
    Original hash: eb393c09dfec331266aa2f8c249a0434a329aa73.

--- samples/linux_clang.sh
+++ samples/linux_clang.sh
@@ -1,1 +1,1 @@
-/usr/local/bin/clang++ -I../include --cuda-gpu-arch=sm_70 -std=c++11 -O2 trie.cu -L/usr/local/cuda/lib64/ -lcudart_static -pthread -ldl -lrt -o trie
+clang++ -I../include --cuda-gpu-arch=sm_70 -std=c++11 -O2 trie.cu -L/usr/local/cuda/lib64/ -lcudart_static -pthread -ldl -lrt -o trie
