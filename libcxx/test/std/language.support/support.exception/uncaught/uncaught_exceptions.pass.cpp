177f4a592d30b18a1ee7e6ecb336ce4d8e130097
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2019-08-27 17:55:50 -0700

    Change uncaught_exceptions.pass.cpp from XFAIL to UNSUPPORTED for old C++ dialects so that it doesn't fail if they happen to have backported it.
    This is the beginning of a history rewrite annotation.
    This is a rewrite of a commit originally made in submodule libcxx.
    Original hash: db08db525d34136baeb50b215ca1469b5d4f3519.

--- libcxx/test/std/language.support/support.exception/uncaught/uncaught_exceptions.pass.cpp
+++ libcxx/test/std/language.support/support.exception/uncaught/uncaught_exceptions.pass.cpp
@@ -17,7 +17,7 @@
 // XFAIL: with_system_cxx_lib=macosx10.12
 // XFAIL: with_system_cxx_lib=macosx10.13
 
-// XFAIL: c++98, c++03, c++11, c++14
+// UNSUPPORTED: c++98, c++03, c++11, c++14
 
 // test uncaught_exceptions
 
