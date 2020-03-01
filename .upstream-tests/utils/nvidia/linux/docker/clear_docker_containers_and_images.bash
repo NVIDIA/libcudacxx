9bad3b164fedd99066611a65bdd4d908ccdd947d
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2020-03-01 13:57:50 -0800

    Docker: Fix `clear_docker_containers_and_images.bash` to correctly pipe all output and errors to `/dev/null`.
    This is the beginning of a history rewrite annotation.
    This is a rewrite of a commit originally made in submodule .upstream-tests.
    Original hash: 87d319af15d06d39fb316ff6bfdd7cc804e7113c.

--- .upstream-tests/utils/nvidia/linux/docker/clear_docker_containers_and_images.bash
+++ .upstream-tests/utils/nvidia/linux/docker/clear_docker_containers_and_images.bash
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-docker container rm $(docker container ls -a | awk '{ print $1 }' | grep -v CONTAINER) 2>&1 /dev/null
-docker image rm $(docker image ls -a | awk '{ print $3 }'  | grep -v IMAGE) 2>&1 /dev/null
+docker container rm $(docker container ls -a | awk '{ print $1 }' | grep -v CONTAINER) > /dev/null 2>&1
+docker image rm $(docker image ls -a | awk '{ print $3 }'  | grep -v IMAGE) > /dev/null 2>&1
 
 exit 0
 
