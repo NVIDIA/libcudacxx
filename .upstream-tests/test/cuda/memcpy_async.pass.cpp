014b005ca9f6a290ed6f20317dace502b40ef17c
Michał 'Griwes' Dominiak <griwes@griwes.info>
2020-03-30 23:31:45 -0700

    Add a test for memcpy_async.
    The test tests for common variable sizes, and then for a large
    structure; it also tests for all combinations of local, global, and
    shared memory, across copy source, copy destination, and the async copy
    barrier to arrive on.
    
    Also modify the testing harness to support having multiple memory space
    selectors for the same element type.
    
    This is the beginning of a history rewrite annotation.
    This is a rewrite of a commit originally made in submodule .upstream-tests.
    Original hash: f6079a203f6bd1750169d5583e9c8928d9c28c50.

---
+++ .upstream-tests/test/cuda/memcpy_async.pass.cpp
@@ -0,0 +1,130 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the CUDA Toolkit, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: pre-sm-70
+
+#include <cuda/barrier>
+
+#include "cuda_space_selector.h"
+
+struct large_type
+{
+    constexpr static int size = 32;
+
+    __host__ __device__
+    large_type(int val = 0) {
+        for (cuda::std::size_t i = 0; i < size; ++i) {
+            storage[i] = val;
+        }
+    }
+
+    large_type(const large_type &) = default;
+    large_type & operator=(const large_type &) = default;
+
+    __host__ __device__
+    friend bool operator==(const large_type & lhs, const large_type & rhs) {
+        for (cuda::std::size_t i = 0; i < size; ++i) {
+            if (lhs.storage[i] != rhs.storage[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    int storage[size];
+};
+
+template <class T,
+    template<typename, typename> class SourceSelector,
+    template<typename, typename> class DestSelector,
+    template<typename, typename> class BarrierSelector
+>
+__host__ __device__ __noinline__
+void test_fully_specialized()
+{
+    SourceSelector<T, constructor_initializer> source_sel;
+    typename DestSelector<T, constructor_initializer>
+        ::template offsetted<decltype(source_sel)::shared_offset> dest_sel;
+    BarrierSelector<cuda::barrier<cuda::thread_scope_block>, constructor_initializer> bar_sel;
+
+    T * source = source_sel.construct(12);
+    T * dest = dest_sel.construct(0);
+    cuda::barrier<cuda::thread_scope_block> * bar = bar_sel.construct(1);
+
+    assert(*source == 12);
+    assert(*dest == 0);
+
+    cuda::memcpy_async(dest, source, sizeof(T), *bar);
+
+    bar->arrive_and_wait();
+
+    assert(*source == 12);
+    assert(*dest == 12);
+
+    *source = 24;
+
+    cuda::memcpy_async(static_cast<void *>(dest), static_cast<void *>(source), sizeof(T), *bar);
+
+    bar->arrive_and_wait();
+
+    assert(*source == 24);
+    assert(*dest == 24);
+}
+
+template <class T,
+    template<typename, typename> class SourceSelector,
+    template<typename, typename> class DestSelector
+>
+__host__ __device__ __noinline__
+void test_select_barrier()
+{
+    test_fully_specialized<T, SourceSelector, DestSelector, local_memory_selector>();
+#ifdef __CUDA_ARCH__
+    test_fully_specialized<T, SourceSelector, DestSelector, shared_memory_selector>();
+    test_fully_specialized<T, SourceSelector, DestSelector, global_memory_selector>();
+#endif
+}
+
+template <class T, template<typename, typename> class SourceSelector>
+struct TestFn {
+    __host__ __device__ __noinline__
+    void operator()() const
+    {
+        test_select_barrier<T, SourceSelector, local_memory_selector>();
+#ifdef __CUDA_ARCH__
+        test_select_barrier<T, SourceSelector, shared_memory_selector>();
+        test_select_barrier<T, SourceSelector, global_memory_selector>();
+#endif
+    }
+};
+
+template <template <class, template<typename, typename> class> class TestFunctor,
+    template<typename, typename> class Selector
+>
+struct TestEachType {
+    __host__ __device__ __noinline__
+    void operator()() const {
+        TestFunctor<  int8_t, Selector>()();
+        TestFunctor<uint16_t, Selector>()();
+        TestFunctor< int32_t, Selector>()();
+        TestFunctor<uint64_t, Selector>()();
+        TestFunctor<large_type, Selector>()();
+    }
+};
+
+int main(int argc, char ** argv)
+{
+    TestEachType<TestFn, local_memory_selector>()();
+#ifdef __CUDA_ARCH__
+    TestEachType<TestFn, shared_memory_selector>()();
+    TestEachType<TestFn, global_memory_selector>()();
+#endif
+
+    return 0;
+}
--- .upstream-tests/test/support/cuda_space_selector.h
+++ .upstream-tests/test/support/cuda_space_selector.h
@@ -6,6 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <cuda/std/cstddef>
+
 #ifdef __CUDACC__
 #define __exec_check_disable__ #pragma nv_exec_check_disable
 #else
@@ -24,17 +26,25 @@
 #define SHARED
 #endif
 
-template<typename T>
+template<typename T, cuda::std::size_t SharedOffset>
 struct malloc_memory_provider {
+    static const constexpr cuda::std::size_t prefix_size
+        = SharedOffset % alignof(T *) == 0
+            ? SharedOffset
+            : SharedOffset + (alignof(T *) - SharedOffset % alignof(T *));
+    static const constexpr cuda::std::size_t shared_offset = prefix_size + sizeof(T *);
+
 private:
     __host__ __device__
     T *& get_pointer() {
+        alignas(T*)
 #ifdef __CUDA_ARCH__
         __shared__
 #else
         static
 #endif
-        T * allocated;
+        char storage[shared_offset];
+        T *& allocated = *reinterpret_cast<T **>(storage + prefix_size);
         return allocated;
     }
 
@@ -44,12 +54,14 @@ public:
 #ifdef __CUDA_ARCH__
         if (threadIdx.x == 0) {
 #endif
-        get_pointer() = reinterpret_cast<T *>(malloc(sizeof(T)));
+        get_pointer() = reinterpret_cast<T *>(malloc(sizeof(T) + alignof(T)));
 #ifdef __CUDA_ARCH__
         }
         __syncthreads();
 #endif
-        return get_pointer();
+        auto ptr = reinterpret_cast<cuda::std::uintptr_t>(get_pointer());
+        ptr += alignof(T) - ptr % alignof(T);
+        return reinterpret_cast<T *>(ptr);
     }
 
     __host__ __device__
@@ -65,8 +77,11 @@ public:
     }
 };
 
-template<typename T>
+template<typename T, cuda::std::size_t SharedOffset>
 struct local_memory_provider {
+    static const constexpr cuda::std::size_t prefix_size = 0;
+    static const constexpr cuda::std::size_t shared_offset = SharedOffset;
+
     alignas(T) char buffer[sizeof(T)];
 
     __host__ __device__
@@ -75,12 +90,18 @@ struct local_memory_provider {
     }
 };
 
-template<typename T>
+template<typename T, cuda::std::size_t SharedOffset>
 struct device_shared_memory_provider {
+    static const constexpr cuda::std::size_t prefix_size
+        = SharedOffset % alignof(T) == 0
+            ? SharedOffset
+            : SharedOffset + (alignof(T) - SharedOffset % alignof(T));
+    static const constexpr cuda::std::size_t shared_offset = prefix_size + sizeof(T);
+
     __device__
     T * get() {
-        __shared__ char buffer[sizeof(T)];
-        return reinterpret_cast<T *>(&buffer);
+        __shared__ alignas(T) char buffer[shared_offset];
+        return reinterpret_cast<T *>(buffer + prefix_size);
     }
 };
 
@@ -109,14 +130,21 @@ struct default_initializer {
 };
 
 template<typename T,
-    template<typename> typename Provider,
-    typename Initializer = constructor_initializer>
+    template<typename, cuda::std::size_t> typename Provider,
+    typename Initializer = constructor_initializer,
+    cuda::std::size_t SharedOffset = 0>
 class memory_selector
 {
-    Provider<T> provider;
+    Provider<T, SharedOffset> provider;
     T * ptr;
 
 public:
+    template<cuda::std::size_t SharedOffset_>
+    using offsetted = memory_selector<T, Provider, Initializer, SharedOffset + SharedOffset_>;
+
+    static const constexpr cuda::std::size_t prefix_size = Provider<T, SharedOffset>::prefix_size;
+    static const constexpr cuda::std::size_t shared_offset = Provider<T, SharedOffset>::shared_offset;
+
 #ifndef __CUDACC_RTC__
     __exec_check_disable__
 #endif
