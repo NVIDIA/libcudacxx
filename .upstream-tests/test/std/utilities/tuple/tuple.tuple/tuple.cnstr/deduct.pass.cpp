721bfd1c9101edaf9a955aafe13c7b3fee373485
Wesley Maxey <wesley.maxey@gmail.com>
2020-11-10 23:24:41 -0800

    Mark CTAD test as expected failure on GCC 10.2

--- .upstream-tests/test/std/utilities/tuple/tuple.tuple/tuple.cnstr/deduct.pass.cpp
+++ .upstream-tests/test/std/utilities/tuple/tuple.tuple/tuple.cnstr/deduct.pass.cpp
@@ -14,7 +14,7 @@
 // GCC's implementation of class template deduction is still immature and runs
 // into issues with libc++. However GCC accepts this code when compiling
 // against libstdc++.
-// XFAIL: gcc-5, gcc-6, gcc-7
+// XFAIL: gcc-5, gcc-6, gcc-7, gcc-10.2
 // UNSUPPORTED: nvrtc
 
 // <cuda/std/tuple>
