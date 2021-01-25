c8b1af5f38c6425b1d33118c23266b86ff48a845
Jake Hemstad <jhemstad@nvidia.com>
2021-01-25 16:55:05 -0600

    Add initial stream_view tests.

---
+++ .upstream-tests/test/cuda/stream/stream_default.pass.cpp
@@ -0,0 +1,21 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <cuda/stream>
+#include <cassert>
+
+int main(int argc, char** argv){
+
+#ifndef __CUDA_ARCH__
+  cuda::stream_view s;
+  assert(s.get() == cudaStream_t{0});
+#endif
+
+  return 0;
+}
