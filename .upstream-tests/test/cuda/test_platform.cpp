64a5fece3bf1a296b569d6e1ad70a4f6100f644f
Wesley Maxey <wesley.maxey@gmail.com>
2021-02-16 18:05:47 -0800

    Update usage in test_platform.cpp

--- .upstream-tests/test/cuda/test_platform.cpp
+++ .upstream-tests/test/cuda/test_platform.cpp
@@ -1,21 +1,38 @@
-#include <cuda/std/version>
+#include <nv/target>
 #include <stdio.h>
 
+template <typename ...>
+__host__ __device__ void test_comma() {}
+
 __host__ __device__ void test() {
-  _LIBCUDACXX_CUDA_DISPATCH(
-      DEVICE,                  _LIBCUDACXX_ARCH_INVOKE([](){printf("device invoked\n");}),
-      SM80,                    _LIBCUDACXX_ARCH_INVOKE([](){printf("sm80 invoked\n");}),
-      LESS_THAN_SM80,          _LIBCUDACXX_ARCH_INVOKE([](){printf("=<sm70 invoked\n");}),
-      GREATER_THAN_SM70,       _LIBCUDACXX_ARCH_INVOKE([](){printf(">sm70 invoked\n");}),
-      HOST,                    _LIBCUDACXX_ARCH_INVOKE([](){printf("host invoked\n");})
+  NV_DISPATCH_TARGET(
+      NV_IS_DEVICE,       (printf("device "); printf("invoked"); printf("\n"); test_comma<short, int, float>();),
+      NV_IS_EXACTLY_SM80, (printf("==sm80 "); printf("invoked"); printf("\n"); test_comma<short, int, float>();),
+      NV_PROVIDES_SM60,   (printf(">=sm60 "); printf("invoked"); printf("\n"); test_comma<short, int, float>();),
+      NV_IS_HOST,         (printf("host ");   printf("invoked"); printf("\n"); test_comma<short, int, float>();)
   )
 
-  _LIBCUDACXX_CUDA_DISPATCH(
-      DEVICE,                  _LIBCUDACXX_ARCH_BLOCK(printf("device "); printf("invoked"); printf("\n");),
-      SM80,                    _LIBCUDACXX_ARCH_BLOCK(printf("sm80 "); printf("invoked"); printf("\n");),
-      LESS_THAN_SM80,          _LIBCUDACXX_ARCH_BLOCK(printf("=<sm70 "); printf("invoked"); printf("\n");),
-      GREATER_THAN_SM70,       _LIBCUDACXX_ARCH_BLOCK(printf(">sm70 "); printf("invoked"); printf("\n");),
-      HOST,                    _LIBCUDACXX_ARCH_BLOCK(printf("host "); printf("invoked"); printf("\n");)
+  NV_DISPATCH_TARGET(
+      NV_IS_EXACTLY_SM80, (printf("==sm80 "); printf("invoked"); printf("\n"); test_comma<short, int, float>();),
+      NV_IS_DEVICE,       (printf("device "); printf("invoked"); printf("\n"); test_comma<short, int, float>();),
+      NV_PROVIDES_SM60,   (printf(">=sm60 "); printf("invoked"); printf("\n"); test_comma<short, int, float>();)
+  )
+
+  NV_DISPATCH_TARGET(
+      NV_PROVIDES_SM60,   (printf(">=sm60 "); printf("invoked"); printf("\n"); test_comma<short, int, float>();),
+      NV_IS_EXACTLY_SM80, (printf("==sm80 "); printf("invoked"); printf("\n"); test_comma<short, int, float>();),
+      NV_IS_DEVICE,       (printf("device "); printf("invoked"); printf("\n"); test_comma<short, int, float>();)
+  )
+
+  NV_IF_TARGET(
+      NV_PROVIDES_SM60,
+      (printf(">=sm60 "); printf("invoked"); printf("\n"); test_comma<short, int, float>();)
+  )
+
+  NV_IF_TARGET(
+      NV_IS_EXACTLY_SM80,
+      (printf("==sm80 "); printf("invoked"); printf("\n"); test_comma<short, int, float>();),
+      (printf("!=sm80 "); printf("invoked"); printf("\n"); test_comma<short, int, float>();)
   )
 }
 
