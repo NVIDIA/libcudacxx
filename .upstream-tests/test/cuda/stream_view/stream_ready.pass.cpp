6ca7df82beac975e8dbe698fa9b6f92238b97742
Jake Hemstad <jhemstad@nvidia.com>
2021-03-01 17:26:54 -0600

    Add test for stream_view::ready.

---
+++ .upstream-tests/test/cuda/stream_view/stream_ready.pass.cpp
@@ -0,0 +1,28 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <cuda/stream_view>
+#include <cassert>
+
+int main(int argc, char** argv){
+
+#ifndef __CUDA_ARCH__
+  cudaStream_t s;
+  cudaStreamCreate(&s);
+  cuda::stream_view sv{s};
+  try{
+    assert(sv.ready());
+  } catch(...) {
+    assert(false && "Should not have thrown");
+  }
+  cudaStreamDestroy(s);
+#endif
+
+  return 0;
+}
