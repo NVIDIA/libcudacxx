d4142784d6e5e3cd3fc5b2e5efd785300bd3e381
Wesley Maxey <wesley.maxey@gmail.com>
2021-04-27 13:51:38 -0700

    <complex>: Force aligning to the total size of both components in complex<T>

---
+++ .upstream-tests/test/std/numerics/complex.number/complex/abi_latest.pass.cpp
@@ -0,0 +1,42 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <cuda/std/complex>
+
+// template<class T>
+// class complex
+// {
+// public:
+//   typedef T value_type;
+//   ...
+// };
+
+#include <cuda/std/complex>
+#include <cuda/std/type_traits>
+
+#include "test_macros.h"
+
+template <class T>
+__host__ __device__ void
+test()
+{
+    typedef cuda::std::complex<T> C;
+
+    static_assert(sizeof(C) == (sizeof(T)*2), "wrong size");
+    static_assert(alignof(C) == (alignof(T)*2), "misaligned");
+}
+
+int main(int, char**)
+{
+    test<float>();
+    test<double>();
+// CUDA treats long double as double
+//  test<long double>();
+
+  return 0;
+}
